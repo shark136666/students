2d72710cc391654845ca95a54f3c2c8afd005609
Mihaylov <paha.mihailov@mail.ru>
2022-06-29 11:47:14 +0500

    les3

--- lesson3/lesson 3 M.2.py
+++ lesson3/lesson 3 M.2.py
@@ -5,14 +5,11 @@ Created on Mon Jun 21 09:28:15 2021
 """
 
 import pyvisa as visa
-import time
 
 
 # Подключение к SNVNA
 rm = visa.ResourceManager()
 rm = visa.ResourceManager('@py')
-#Connect to a Socket on the local machine at 5025
-#Use the IP address of a remote machine to connect to it instead
 try:
     CMT = rm.open_resource('TCPIP0::localhost::5025::SOCKET')
 except:
@@ -23,23 +20,30 @@ except:
 CMT.read_termination='\n'
 #Set a really long timeout period for slow sweeps
 CMT.timeout = 100000
-##############################################
-# Есть два типа запросов 
-# первый это отправить данные в программу
-CMT.write(f'')
-# примеры использования
 trace_count = 1
+"""
+print ("Введите колличество трасс")
+n = int(input(int)) + 1
+"""
 n = 17
-CMT.write(f'')
 while trace_count < n:    
-    CMT.write(f'CALC1:PAR:COUN {trace_count}')                              #создает трассу
+    CMT.write(f'CALC1:PAR:COUN {trace_count}')                              # создает трассу
+    
     CMT.write(f'CALC1:PAR{trace_count}:DEF S{trace_count}{trace_count}')    # меняет S
-    CMT.write(f'CALC1:TRAC{trace_count}:MARK1:STAT ON;:CALC1:TRAC{trace_count}:MARK2:STAT ON;:CALC1:TRAC{trace_count}:MARK1:X 3e9')#;:CALC1:TRAC1:MARK2:X 35e8
+
+    CMT.write(f'CALC:PAR{trace_count}:SEL')                                 # выбор трассы
+    
+    CMT.write(f'CALC1:TRAC{trace_count}:MARK1:STAT ON;:')                   # создание 1 маркера
+    CMT.write(f'CALC1:TRAC{trace_count}:MARK2:STAT ON')                     # создание 2 маркера
+    CMT.write(f'CALC1:MARK1:X 1e9')                                         # смещение на  1 GHz
+    CMT.write(f'CALC1:MARK2:X 55e8')                                        # смещение на  5.5 GHz
+    
     trace_count += 1
 
-f = input()
+f = input()                                                                 # прост для ожидания
 trace_count = 1
-while trace_count < n: # изменение  S{trace_count}{trace_count-1} у четных
+
+while trace_count < n:                                                      # изменение  S{trace_count}{trace_count-1} у четных
     if trace_count % 2 == 0:
         CMT.write(f'CALC1:PAR{trace_count}:DEF S{trace_count}{trace_count-1}')  # меняет S
         trace_def = CMT.query(f'CALC1:PAR{trace_count}:DEF?')
@@ -49,9 +53,8 @@ while trace_count < n: # изменение  S{trace_count}{trace_count-1} у ч
         print("трасса №",trace_count," = S",trace_def)
     trace_count += 1
 
-
-#второй это получение данных из программы, он возвращает значения, их можно добавлять в переменные
 trace_count = CMT.query(f'CALC1:PAR:COUN?')
-print("Трасс ",trace_count)
+
+print("Всего трасс ",trace_count)                                       
 
 
