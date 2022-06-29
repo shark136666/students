ddc0835c721abde1cf2eb4e2f6574277159c0f9b
ShchegolevAV <njznew@gmail.com>
2022-06-29 11:25:26 +0500

    les3

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -25,14 +25,14 @@ CMT.read_termination='\n'
 #Set a really long timeout period for slow sweeps
 CMT.timeout = 100000
 ##############################################
+#Пресет
+CMT.write(f'SYST:PRES')
 
-#Добавить 16 трасс в канал ->
+#Добавляем 16 трасс в канал 
 CMT.write(f'CALC1:PAR:COUN 16')
 #Выводим на консоль
 trace_count = CMT.query(f'CALC1:PAR:COUN?')
-print(trace_count)
-
-
+print(f'кол-во трасс: {trace_count}')
 
 #Привести трассы к виду 1 трасса s11, 2 трасса s22...16 трасса s1616
 for i in range(1,17):
@@ -40,13 +40,25 @@ for i in range(1,17):
 
 #На каждую трассу добавить 2 маркера 1Ghz и 5.5 Ghz
 for i in range(1,17):
-	CMT.write(f'CALC1:TRAC{i}:MARK1:STAT ON;:CALC1:TRAC{i}:MARK2:STAT ON;:CALC1:TRAC{i}:MARK1:X 1e9;:CALC1:TRAC{i}:MARK2:X 55e8')
+	#Выбираем активную трассу
+	CMT.write(f'CALC1:PAR{i}:SEL')
+	
+	#Включаем марки 1 и 2 
+	CMT.write(f'CALC1:TRAC{i}:MARK1:STAT ON')
+	CMT.write(f'CALC1:TRAC{i}:MARK2:STAT ON')
+
+	#Выставляем 1Ghz и 5.5 Ghz
+	CMT.write(f'CALC1:MARK1:X 1e9')
+	CMT.write(f'CALC1:MARK2:X 55e8')
 	
 #Каждую четную трассу приветси к виду S номер трассы (номер трассы-1)
 for i in range(1,17):
+	
+	#Изменяем четную трассу
 	if i % 2 == 0:
 		n = i-1
 		CMT.write(f'CALC1:PAR{i}:DEF S{i}{n}')
+	#Запрашиваем данные и выводим
 	trace_def = CMT.query(f'CALC1:PAR{i}:DEF?')
 	print("трасса №",i,"= ",trace_def)
 	
