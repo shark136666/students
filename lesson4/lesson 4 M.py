cc64e0b61a33b6deb570fb4c1f4b5c6602026280
Mihaylov <paha.mihailov@mail.ru>
2022-07-04 14:31:59 +0500

    test

--- lesson4/lesson 4 M.py
+++ lesson4/lesson 4 M.py
@@ -22,6 +22,21 @@ CMT.read_termination='\n'
 #Set a really long timeout period for slow sweeps
 CMT.timeout = 100000
 
+
+def dicorate(func):
+    def wrapper(connect, command):
+        print("start")
+        func(connect, command)
+        err = connect.query(f'SYST:ERR?')
+        if  err != "0, No error":
+            print(err)
+        print("end")
+    return wrapper
+
+
+
+
+
 # функции
 def convert (m, f):
     if  f == "GHz":
@@ -32,10 +47,8 @@ def convert (m, f):
         m = m * 1000000
     return m
 
-def ERR(connect):
-    err = connect.query(f'SYST:ERR?')
-    if  err != "0, No error":
-       print(err)
+
+    
    
 def scan(connect):
     connect.write(f'TRIG:SOUR BUS')
@@ -46,96 +59,10 @@ def scan(connect):
     ERR(CMT)
     connect.query(f'*OPC?')
     ERR(CMT)
+
+@dicorate
+def write (connect, command):
+    connect.write(command)
     
-
-#Пресет
-CMT.write(f'SYST:PRES')
-
-# частота начала и конца росчерка
-CMT.write(f'SENS:FREQ:STAR {convert(1, "GHz")}')
-freq_star = CMT.query(f'SENS:FREQ:STAR?')
-CMT.write(f'SENS:FREQ:STOP {convert(6, "GHz")}')
-freq_stop = CMT.query(f'SENS:FREQ:STOP?')
-
-# Установка кол-ва точек 
-CMT.write(f'SENS:SWE:POIN 1001')
-point = CMT.query(f'SENS:SWE:POIN?')
-
-#Установка фильтра ПЧ
-CMT.write(f'SENS:BAND {convert(3, "kHz")}')
-band = CMT.query(f'SENS:BAND?')
-time.sleep(1)
-
-print ("Канал №1 start ", freq_star, "Gz | stop ", freq_stop, "Gz | point ", point, "| band ", band)
-
-# создание маркеров
-CMT.write(f'CALC:MARK1:STAT ON')
-CMT.write(f'CALC:MARK2:STAT ON')
-CMT.write(f'CALC:MARK3:STAT ON')
-CMT.write(f'CALC:MARK4:STAT ON')
-
-# раставление маркеров
-CMT.write(f'CALC1:MARK1:X {convert(2, "GHz")}')
-CMT.write(f'CALC1:MARK2:X {convert(3, "GHz")}')
-CMT.write(f'CALC1:MARK3:X {convert(4, "GHz")}')
-CMT.write(f'CALC1:MARK4:X {convert(5, "GHz")}')
-
-# вкл статистику и номера рабочих маркеров 
-CMT.write(f'CALC:MST ON')
-CMT.write(f'CALC:MST:DOM ON')
-
-Dict = {}
-
-for i in range(1,5):
-    if i == 4:
-        CMT.write(f'CALC:MST:DOM:STAR {1}')
-        ERR(CMT)
-        CMT.write(f'CALC:MST:DOM:STOP {i}')
-        ERR(CMT)
-    else:
-        CMT.write(f'CALC:MST:DOM:STAR {i}')
-        ERR(CMT)
-        CMT.write(f'CALC:MST:DOM:STOP {i+1}')
-        ERR(CMT)
-        
-    a = CMT.query(f'CALC:MST:DATA?').split(',')
-    b = CMT.query(f'CALC:MST:DOM:STAR?')
-    c = CMT.query(f'CALC:MST:DOM:STOP?')
-    Dict.update({f'{b} - {c}':{f'mean':f'{a[0]}', f's.dev':f'{a[1]}',f'p-p':f'{a[2]}'}})
-for key, value in Dict.items():
-	print(f'Маркеры {key}\n Среднее значение = {value["mean"]}\n Стандартное отклонение = {value["s.dev"]}\n Фактор пик-пик = {value["p-p"]}')
-
-print('')
-#Пресет
-CMT.write(f'SYST:PRES')
-
-#смена на S
-CMT.write(f'CALC1:PAR1:DEf S21')
-
-# вкл мат статистику
-CMT.write(f'CALC:MST ON')
-
-#однократный росчерк 
-scan(CMT)
-
-#Вывести значение mean
-a = CMT.query(f'CALC:MST:DATA?').split(',')
-print("mean = ",a[0])
-
-#вкл усреднение
-CMT.write(f'SENS:AVER ON')
-
-#устанавливаем фактор усреднение
-CMT.write(f'SENS:AVER:COUN 100')
-
-print("Выполняем ",CMT.query(f'SENS:AVER:COUN?'),"росчерков ")
-# выполнение 100 росчерков
-for i in CMT.query(f'SENS:AVER:COUN?'):
-    scan(CMT)
-
-# вывод mean
-a = CMT.query(f'CALC:MST:DATA?').split(',')
-print("mean = ",a[0])
-
-
-
+write(CMT, f'SENS:FREQ:STAR {convert(1, "GHz")}')
+write(CMT, f'SENS:FREQ:STAR {convert(1, "GHz")}')
