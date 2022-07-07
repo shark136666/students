2960ec39d95509f01d8b218623751d6b7c5e1283
SchchegolevAV <njznew@gmail.com>
2022-07-07 11:32:24 +0500

    sko_table ver2

--- lesson5/sko_table.py
+++ lesson5/sko_table.py
@@ -7,7 +7,7 @@ data = {}
 with open('sko_result.json', 'r', encoding='utf-8') as fh: 
     data.update(json.load(fh))
 
-dop = float(3)
+dop = float(input())
 	
 htmlstr = "<!-- Bootstrap CSS -->\
     <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>\
@@ -38,17 +38,23 @@ for i in data["atthenuator"].keys():
 						htmlstr += f'<tr><td>{n}</td>'
 						l = data["atthenuator"][f'{i}']["IF"][f'{k}']["trace"][f'{j}'].keys()
 						htmlstr += f'<td rowspan={len(l)-1}>[{k}]</td><td rowspan={len(l)-1}>[{i}]</td>'
-						a = data["atthenuator"][f'{i}']["IF"][f'{k}']["trace"][f'{j}'][f'{n}']
 						max_value = data["atthenuator"][f'{i}']["IF"][f'{k}']["trace"][f'{j}']["max_value"]
-						htmlstr += f'<td>[{a}]</td><td rowspan={len(l)-1} >[{max_value}]</td>'
-						
-						if max_value > dop:
-							htmlstr += f'<td rowspan={len(l)-1} class = bg-warning >[{dop}]</td>'
+						a = data["atthenuator"][f'{i}']["IF"][f'{k}']["trace"][f'{j}'][f'{n}']
+						if a > dop:
+							htmlstr += f'<td class = bg-warning>[{a}]</td><td rowspan={len(l)-1} >[{max_value}]</td>'
 						else:
-							htmlstr += f'<td rowspan={len(l)-1}>[{dop}]</td>'
+							htmlstr += f'<td>[{a}]</td><td rowspan={len(l)-1} >[{max_value}]</td>'
+						
+						htmlstr += f'<td rowspan={len(l)-1}  >[{dop}]</td>'
+						
+						
 					else:
 						a = data["atthenuator"][f'{i}']["IF"][f'{k}']["trace"][f'{j}'][f'{n}']
-						htmlstr += f'<tr><td>{n}</td><td >[{a}]</td>'
+						if a > dop:
+							htmlstr += f'<tr><td>{n}</td><td class = bg-warning>[{a}]</td>'
+						else:
+							htmlstr += f'<tr><td>{n}</td><td >[{a}]</td>'
+						
 						
 					
 				
