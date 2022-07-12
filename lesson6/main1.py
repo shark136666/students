62ab6350ac253e8b65997607c80963957c20a65c
Mihaylov <paha.mihailov@mail.ru>
2022-07-12 16:50:40 +0500

    lesson ver 3

--- lesson6/main1.py
+++ lesson6/main1.py
@@ -13,6 +13,7 @@ conf_data = {
 #словарь для записи в json
 jsondict = {}
 
+
 CMT = SNVNAopen()
 
 #Функция чтения файла config.txt и записи его значений
@@ -62,10 +63,28 @@ def double_point():
 	write(CMT, str)
 
 #Функция проведения тестов и записи в json
+
+
 def start_frequency_test(conf_data):
+
+        #date_inf = query(CMT,f'SYST:DATE?')
+        time_inf = query(CMT,f'SYST:TIME?')
+        idn_inf = list(query(CMT,f'*IDN?'))
+
+        #print(date_inf)
+        print(time_inf)
+        idn_inf = "".join(idn_inf).split(", ")
+        jsondict.update({"inf":{}})
+        jsondict["inf"].update({"time":f'{time_inf}'})
+        jsondict["inf"].update({"device":f'{idn_inf[0]}'})
+        jsondict["inf"].update({"model":f'{idn_inf[1]}'})
+        jsondict["inf"].update({"serial":f'{idn_inf[2]}'})
+        jsondict["inf"].update({"version":f'{idn_inf[3]}'})
+        
         #Подключаемся к SNVNA
+        k = 1
         """ ввод колличества потоков """
-        tracc_count = 16
+        tracc_count = 3
         if int(query(CMT,f'SERV:PORT:COUN?')) < tracc_count:
                 tracc_count = int(query(CMT,f'SERV:PORT:COUN?'))
         
@@ -73,56 +92,95 @@ def start_frequency_test(conf_data):
         double_point()
         config = configparser.ConfigParser()
         config.read_file(open(r'config.txt'))
-        #цикл прохождения по диапозонам
-        for keys in range(1,int(len(conf_data['range'].keys())/2)+1): 
-                start = conf_data["range"][f'{keys}_start']
-                stop = conf_data["range"][f'{keys}_stop']
+        #цикл вывода otn
+
+
+        for keys in range(1,int(len(conf_data['range'].keys())/2)+1):
                 start_data = config["range"][f'{keys}_start']
                 stop_data = config["range"][f'{keys}_stop']
-                jsondict.update({f'{start_data} - {stop_data}' : {}})
-                jsondict[f'{start_data} - {stop_data}'].update({'absolute' : {},'otnosit' : {}})
-                max_p_p_otn = 0
-                max_p_p_abs = 0
+                jsondict.update ({f'{start_data} - {stop_data}':{}})
+                jsondict[f'{start_data} - {stop_data}'].update({'max_abs' : 0.0,'max_otn' : 0.0})
+                
 
-                for canal in range(1,tracc_count+1):
-                    #создание 3 трасс
-                    write(CMT, f'CALC1:PAR:COUN 3')
-                    write(CMT, f'CALC1:PAR1:DEF S{canal}{canal}')
-                    write(CMT, f'CALC1:PAR2:DEF R{canal}')
-                    write(CMT, f'CALC1:PAR2:SPOR {canal}')
-                    write(CMT, f'CALC1:PAR3:DEF T{canal}')
-                    write(CMT, f'CALC1:PAR3:SPOR {canal}')
-                    singlescan(CMT)
-                    #--------------------
-                                
-                    for n_trace  in range(1,4):
-                        write(CMT, f'CALC:PAR{n_trace}:SEL')
-                        write(CMT, f'CALC1:TRAC{n_trace}:MARK1:STAT ON')
-                        write(CMT, f'CALC1:TRAC{n_trace}:MARK2:STAT ON')
+        for canal in range(1,tracc_count+1):
+                #создание 3 трасс
+                write(CMT, f'CALC1:PAR:COUN 1')
+                write(CMT, f'CALC1:PAR1:DEF S{canal}{canal}')
+                singlescan(CMT)
+                #--------------------
+                
+                for keys in range(1,int(len(conf_data['range'].keys())/2)+1): 
+                        start = conf_data["range"][f'{keys}_start']
+                        stop = conf_data["range"][f'{keys}_stop']
+                        start_data = config["range"][f'{keys}_start']
+                        stop_data = config["range"][f'{keys}_stop']
+                        
+
+                        write(CMT, f'CALC1:TRAC:MARK1:STAT ON')
+                        write(CMT, f'CALC1:TRAC:MARK2:STAT ON')
                         write(CMT, f'CALC1:MARK1:X {start}')
                         write(CMT, f'CALC1:MARK2:X {stop}')
                         write(CMT, f'CALC1:MST ON')
                         write(CMT, f'CALC1:MST:DOM ON')
                         
-
                         mst_data = query_ascii_values(CMT,f'CALC:MST:DATA?')
-                        trace = query(CMT, f'CALC1:PAR{n_trace}:DEF?')
-                        if  "S" in trace:
-                                jsondict[f'{start_data} - {stop_data}']["otnosit"].update({f'{trace}': mst_data[2] })
-                                if max_p_p_otn < float(mst_data[2]) or max_p_p_otn == 0:
-                                        max_p_p_otn = float(mst_data[2])
-                                        print(max_p_p_otn)
-                        else:
-                                jsondict[f'{start_data} - {stop_data}']["absolute"].update({f'{trace}': mst_data[2] })
-                                if max_p_p_abs < float(mst_data[2]) or max_p_p_abs == 0:
-                                        max_p_p_abs = float(mst_data[2])
-                                        print(max_p_p_abs)
-                jsondict[f'{start_data} - {stop_data}']["otnosit"].update({'max_value': max_p_p_otn})
-                jsondict[f'{start_data} - {stop_data}']["absolute"].update({'max_value': max_p_p_abs})
-        print(jsondict)
-        
-	
+                        trace = query(CMT, f'CALC1:PAR:DEF?')
+                        if k < 2 :
+                                jsondict.update({f'{trace}': {}})
+                                k += 1
+                        jsondict[f'{trace}'].update({f'{start_data} - {stop_data}':{}})
+                        print(jsondict)
+                        if mst_data[2] > jsondict[f'{start_data} - {stop_data}']["max_otn"] or jsondict[f'{start_data} - {stop_data}']["max_otn"] == 0.0 :
+                                jsondict[f'{start_data} - {stop_data}'].update({"max_otn" : mst_data[2]})
+                        jsondict[f'{trace}'][f'{start_data} - {stop_data}'].update({'Value':mst_data[2]})
+
+                k = 1      
+              
+
+        #цикл вывода abs
+        for canal in range(1,tracc_count+1):
+                #создание 3 трасс
+                write(CMT, f'CALC1:PAR:COUN 2')
+                write(CMT, f'CALC1:PAR1:DEF R{canal}')
+                write(CMT, f'CALC1:PAR1:SPOR {canal}')
+                write(CMT, f'CALC1:PAR2:DEF T{canal}')
+                write(CMT, f'CALC1:PAR2:SPOR {canal}')
+                singlescan(CMT)
+                #--------------------
+
+                for keys in range(1,int(len(conf_data['range'].keys())/2)+1): 
+                        start = conf_data["range"][f'{keys}_start']
+                        stop = conf_data["range"][f'{keys}_stop']
+                        start_data = config["range"][f'{keys}_start']
+                        stop_data = config["range"][f'{keys}_stop']
+
+
+                        for n_trace in range(1,3):
+                                write(CMT, f'CALC:PAR{n_trace}:SEL')
+                                write(CMT, f'CALC1:TRAC{n_trace}:MARK1:STAT ON')
+                                write(CMT, f'CALC1:TRAC{n_trace}:MARK2:STAT ON')
+                                write(CMT, f'CALC1:MARK1:X {start}')
+                                write(CMT, f'CALC1:MARK2:X {stop}')
+                                write(CMT, f'CALC1:MST ON')
+                                write(CMT, f'CALC1:MST:DOM ON')
+
+                                mst_data = query_ascii_values(CMT,f'CALC:MST:DATA?')
+                                trace = query(CMT, f'CALC1:PAR{n_trace}:DEF?')
+                                if k < 3 :
+                                        jsondict.update({f'{trace}': {}})
+                                        k += 1
+                                jsondict[f'{trace}'].update({f'{start_data} - {stop_data}':{}})
+                                if mst_data[2] > jsondict[f'{start_data} - {stop_data}']["max_abs"] or jsondict[f'{start_data} - {stop_data}']["max_abs"] == 0.0 :
+                                        jsondict[f'{start_data} - {stop_data}'].update({"max_abs" : mst_data[2]})
+                                jsondict[f'{trace}'][f'{start_data} - {stop_data}'].update({'Value':mst_data[2]})
+                               
+
+                k = 1                          
+
+
+
 readconfig()
 start_frequency_test(conf_data)
 with open("p-p_result.json", "w") as write_file:
 	json.dump(jsondict, write_file, indent=2)
+	
--- lesson6/test.py
+++ lesson6/test.py
@@ -1,15 +1,15 @@
 import json
-
+from connect import SNVNAopen, write, query, query_ascii_values, HzConvertor, singlescan
 #создаем словарь для записи с json
 data = {}
 
 #открываем файл и загружаем данные
 with open('p-p_result.json', 'r', encoding='utf-8') as fh: 
     data.update(json.load(fh))
-
+print(data)
 #ввод допуска
-dop_abs = 2
-dop_otn = 3
+dop_abs = 5
+dop_otn = 6
 #dop_abs = float(input("Введите значение допуска abs "))
 #dop_otn = float(input("Введите значение допуска otn "))
 #создаем строку для ввода в html страницу
@@ -24,18 +24,103 @@ th, td {\
     padding: 1px;;\
 }\
     </style>\
-	<table border='1' >\
 "
+htmlstr +="<table border='1' ><tr><td> Время запуска </td><td>  device </td><td> model </td><td> serial </td><td>  version </td></tr><tr>"
+for i in data["inf"].keys():
+    htmlstr +=f'<td>{data["inf"][i]}</td>'
+
+
+
+
+htmlstr +="<table border='1' ><tr><td> Диапазон </td>"
 #Цикл для заолнения html страницы
+print(list(data.keys()))
+for i in list(data.keys()):
+    if i == "S11":
+        break
+    if i != "inf":
+        htmlstr += f'<td colspan="3">{i}</td>'
+htmlstr += "</tr> <tr><td>Трассы</td>  <td>Нер-ть,дБ</td>  <td>Макс.дБ</td>    <td>Допуск,дБ</td> <td>Нер-ть,дБ</td>  <td>Макс.дБ</td>    <td>Допуск,дБ</td></tr><tr>"
+k=0
+for trace in data.keys():
+    if trace == "S11"or k == 3:
+        k = 3
+        htmlstr += f'<td>{trace}</td> '
+        for Range in data[f'{trace}'].keys():
+            value = data[f'{trace}'][f'{Range}']["Value"]
+            if "S" in  f'{trace}':
+                """ otnositelnie """
+                if value == data[f'{Range}']["max_otn"]:
+                     htmlstr +=f'<td>{value}</td> '     
 
-for trace in data[Range]["otnosit"].keys():
-    for Range in data.keys():
+                     if value > dop_otn:
+                        htmlstr +=f'<td class = bg-warning> {value} </td> <td class = bg-warning> {dop_otn} </td> '
+                     else:
+                         htmlstr +=f'<td> {value} </td> <td> {dop_otn} </td>'
+                else:
+                    htmlstr +=f'<td> {value} </td> <td> - </td><td>{dop_otn}</td>'
+                """ absolute """
+            else:
+                if value == data[f'{Range}']["max_abs"]:
+                     htmlstr +=f'<td>{value}</td> '
+
+                     if value > dop_abs:
+                        htmlstr +=f'<td class = bg-warning> {value} </td> <td class = bg-warning> {dop_abs} </td> '
+                     else:
+                         htmlstr +=f'<td> {value} </td> <td> {dop_abs} </td>'
+                else:
+                    htmlstr +=f'<td> {value} </td> <td> - </td> <td>{dop_abs}</td>'
+        htmlstr += "</tr><tr>"
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+"""
 for Range in data.keys():
     htmlstr += f'<th><tr><td>Диапазон</td><td colspan="3">{Range}</th>'
-    htmlstr += "<tr><td>Трассы</td>  <td>Нер-ть,дБ</td>  <td>Макс.дБ</td>    <td>Допуск,дБ</td> </tr><tr> "
-    """ ---цикл прохода по otn--- """
+    htmlstr += "<tr><td>Трассы</td>  <td>Нер-ть,дБ</td>  <td>Макс.дБ</td>    <td>Допуск,дБ</td> <td><td>Нер-ть,дБ</td>  <td>Макс.дБ</td>    <td>Допуск,дБ</td></tr><tr> "
+    htmlstr +=
+    ---цикл прохода по otn--- 
     for trace in data[Range]["otnosit"].keys():
         if trace != "max_value":
             value = data[Range]["otnosit"][f'{trace}']
@@ -53,7 +138,7 @@ for Range in data.keys():
         
 
 
-    """ ---цикл прохода по abs--- """      
+     ---цикл прохода по abs---     
     for trace in data[Range]["absolute"].keys():  
         if trace != "max_value":
             value = data[Range]["absolute"][f'{trace}']
@@ -69,6 +154,7 @@ for Range in data.keys():
                 htmlstr += "<td>-</td>"
                 htmlstr += f'<td> {dop_abs} </td> <tr>'
     htmlstr += "<th>"
+"""
 #записываем в файл			
 with open('html_table.html', 'w', encoding='utf-8') as fh1: 
      fh1.write(htmlstr)
